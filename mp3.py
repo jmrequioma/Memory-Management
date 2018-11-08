3b6732ad3853e228e49f812822186380102570c6
jmrequioma <jerome.requioma@gmail.com>
2018-11-08 11:41:18 +0800

    make array of jobs

--- mp3.py
+++ mp3.py
@@ -1,14 +1,28 @@
-class Job:
-	def __init__(self, kind):
-		self.kind = kind
+class Job(object):
+	def __init__(self, job_stream_num, time, job_size):
+		self.job_stream_num = job_stream_num
+		self.time = time
+		self.job_size = job_size
+	
+def readJobFile():
+	with open("jobs.in", "r") as ins:
+		arr = []
+		jobs = []
+		for line in ins:
+			arr.append(line)
+			separated = line.split(';')
+			job = Job(separated[0], separated[1], separated[2])
+			jobs.append(job)
 
-	def whatKind(self):
-		return self.kind
+	return jobs
+
+
+
 def main():
 	print("Hello World!")
-	count = Job("Count")
-	read = Job("Read")
-	print(read.whatKind())
+	jobs = readJobFile()
+	for job in jobs:
+		print(job.job_stream_num)
 
 if __name__ == '__main__':
 	main()
